dbadc16aec433b821c0e9f6877280a39221b0124
Cheto02 <fredycheto@gmail.com>
2023-08-10 04:43:59 -0700

    with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	/* Print a message */
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
-}
+        /* Return 0 */
+    return (0);
+
