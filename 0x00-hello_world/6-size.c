724944ea83cc5bef6dad84055bdb3412e7453003
Cheto02 <fredycheto@gmail.com>
2023-08-13 05:17:49 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
-#include<limits.h>
 #include<stdio.h>
 /*
- * main - Entry point
- *
+ * main - prints the size of various types on the computer it is compiled and
+ * run on.
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,7 +12,7 @@ int main(void)
 	long long int d;
 	float f;
 printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %zu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a longint: %zu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a longlong int: %zu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
