54f656d13f82e77d89449381f65f09bd27b31d19
Cheto02 <fredycheto@gmail.com>
2023-08-13 05:31:01 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-/*
+/**
  * main - prints the size of various types on the computer it is compiled and
- * run on.
+ * run on..
  * Return: Always 0 (Success)
  */
 int main(void)
