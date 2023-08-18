23a816c57eab62f043bbfa4e13efb3474efb439b
Cheto02 <fredycheto@gmail.com>
2023-08-18 07:10:50 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,16 +3,16 @@
 /**
  * _isupper - checks for uppercase character
  * @c: Variable text
- * Return: Always 0
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
