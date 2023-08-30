4af74d936b545a752518b56d7ab2acab6eb09fa0
Cheto02 <fredycheto@gmail.com>
2023-08-30 13:31:26 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
+int _putchar(char c);
 
 int find_sqrt(int n, int guess);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
- *
  * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
@@ -17,6 +17,7 @@ int _sqrt_recursion(int n)
 	else
 	return (find_sqrt(n, 1));
 }
+/* @find_sqrt- a function that recursively finds the square root*/
 
 int find_sqrt(int n, int guess)
 {
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * is_prime_number- a function that returns 1 if the input integer is a prime
+ * number.
+ * is_prime_helper- a function that check whether a number is prime
+ * @n: number to evaluate
+ * @divisor: checking for divisibility
+ * Return: 1 if n is a prime number, 0 if not.
+ */
+int is_prime_helper(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (divisor == 1)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+		return (is_prime_helper(n, divisor - 1));
+}
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+		return (is_prime_helper(n, n - 1));
+}
+
+
+
+
