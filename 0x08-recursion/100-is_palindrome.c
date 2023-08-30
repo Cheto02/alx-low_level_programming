814796fab17c15f03b47871167a1a659716bdf12
Cheto02 <fredycheto@gmail.com>
2023-08-30 14:32:05 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+		return (check_palindrome(s, start + 1, end - 1));
+}
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+		return 1 + _strlen_recursion(s + 1);
+}
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+	return (check_palindrome(s, 0, len - 1));
+}
