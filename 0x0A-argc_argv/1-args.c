9fc34fa5ce46cb94d78e161630250239b084a853
Cheto02 <fredycheto@gmail.com>
2023-09-01 16:11:59 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ * @argc:integer
+ * @argv:array of strings
+ *Return: 0 as success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
+
