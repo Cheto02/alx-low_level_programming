3df285355b8f69df81b6cb617c0644f84050f04b
Cheto02 <fredycheto@gmail.com>
2023-09-06 06:14:50 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,22 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
-
+	/*  a loop to change the first n bytes of the memory block
+	 *  pointed to by s to the value of b
+	 *
+	 *  The loop continues as long as the value of n is greater than 0.
+	 *  In each iteration of the loop:
+	 *  The value at the memory location pointed to by s + i (i.e.,
+	 *  the current position in the memory block) is set to the value b.
+	 *  The n variable is decremented by 1
+	 *  The i variable is incremented by 1, effectively moving
+	 *  to the next byte in the memory block.
+	 */
 	for (; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 	}
-	return (s);
+	return (s); /* returns the changed array with the new value for n bytes */
 }
 
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,18 +2,26 @@
 #include "main.h"
 /**
  * _memcpy - a function that copies memory area.
- * @dest: memory where is stored
- * @src: memory where it is copied
- * *@n: number of bytes
+ * @dest: memory where is stored or destination
+ * @src: memory where it is copied from or sourced
+ * *@n: number of bytes copied from src to dest
  * Return: copied memory with n bytes
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
-
+	/*  This loop will copy data n times (as long as r is less than i */
 	for (; r < i; r++)
 	{
+	/**
+	 * The value at the memory location pointed to by src + r
+	 * (i.e., the current position in the source memory) is assigned
+	 * to the memory location pointed to by dest + r (i.e., the current
+	 * position in the destination memory). This effectively copies a byte
+	 * from src to dest.The n variable is decremented by 1 in
+	 * each iteration, indicating that one byte has been copied.
+	 */
 	dest[r] = src[r];
 	n--;
 	}
