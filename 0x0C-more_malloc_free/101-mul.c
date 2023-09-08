b003d908f505b11f06011c2adbec55ec2e2445b7
Cheto02 <fredycheto@gmail.com>
2023-09-08 14:20:47 -0700

    101-mul.c git commit -m101-mul.c git commit -m101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,46 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-/**
- * mul -  a program that multiplies two positive numbers.
- * @num1:first number to multiply
- * @num2: second number to multiply
- *Return: result of mul
- */
-unsigned int mul(unsigned int num1, unsigned int num2)
-{
-	unsigned int result = num1 * num2;
+#include <stdlib.h>
 
-	return (result);
-}
 /**
- *main- program entry point
- *@argc: argument count
- *@argv: argument vector
- *Return:98 on error and 0 on success
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing the arguments
+ *
+ * Return: 0 on success, 98 on failure
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	unsigned int num1, num2, product;
+	int len1, len2, i, j;
+	char *num1, *num2, *result;
 
 	if (argc != 3)
+		return (printf("Error\n"), 98);
+	num1 = argv[1], num2 = argv[2];
+	len1 = strlen(num1), len2 = strlen(num2);
+	for (i = 0; i < len1; i++)
+	if (num1[i] < '0' || num1[i] > '9')
+		return (printf("Error\n"), 98);
+	for (i = 0; i < len2; i++)
+	if (num2[i] < '0' || num2[i] > '9')
+		return (printf("Error\n"), 98);
+	result = calloc(len1 + len2 + 1, sizeof(char));
+	if (!result)
+		return (98);
+	for (i = len1 - 1; i >= 0; i--)
+	for (j = len2 - 1; j >= 0; j--)
+	result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+	for (i = len1 + len2 - 1; i > 0; i--)
 	{
-	printf("Error\n");
-	return (98);
+	result[i - 1] += result[i] / 10;
+	result[i] %= 10;
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	if (num1 == 0 || num2 == 0)
-	{
-	printf("Error\n");
-	return (98);
-	}
-
-	product = mul(num1, num2);
-	printf("%u\n", product);
-
+	for (i = 0; i < len1 + len2 && result[i] == 0; i++)
+	if (i == len1 + len2)
+	_putchar('0');
+	else
+	for (; i < len1 + len2; i++)
+	_putchar(result[i] + '0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,11 +3,14 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-unsigned int mul(unsigned int num1, unsigned int num2);
 #endif
