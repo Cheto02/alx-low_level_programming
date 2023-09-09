644addc1b8d57ed3d6ea0a9124a0df29ddfe3897
Cheto02 <fredycheto@gmail.com>
2023-09-09 12:42:29 -0700

    fixed minmax error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,17 +6,18 @@
  *
  *Return: the pointer to the newly created array
  */
-
 int *array_range(int min, int max)
 {
-	int size = max - min + 1;
-	int *arr = malloc(size * sizeof(int));
-	int i;
+	int size, i;
+	int *arr;
 
 	if (min > max)
 	{
 	return (NULL);
 	}
+
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
 
 	if (arr == NULL)
 	{
@@ -27,6 +28,7 @@ int *array_range(int min, int max)
 	{
 	arr[i] = min++;
 	}
+
 	return (arr);
 }
 
