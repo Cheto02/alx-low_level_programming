ccb18cefb8654e5d2ae3326fd05a2d85e154501c
Cheto02 <fredycheto@gmail.com>
2023-09-09 12:56:14 -0700

    fixed unused variable error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,37 +1,42 @@
 #include <stdlib.h>
 #include <string.h>
+
 /**
- *_realloc- a function that reallocates a memory block using malloc and free
- *@ptr:a pointer to the memory previously allocated
- *@old_size:size, in bytes, of the allocated space for ptr
- *@new_size:new size, in bytes of the new memory block
- *Return: Return NULL
+ * _realloc - a function that reallocates a memory block using malloc and free
+ * @ptr: a pointer to the memory previously allocated
+ * @old_size: size, in bytes, of the allocated space for ptr
+ * @new_size: new size, in bytes of the new memory block
+ *
+ * Return: pointer to newly allocated memory or NULL if failure
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	char *new_ptr;
 
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr == NULL)
-	{
-	return (malloc(new_size));
-	}
-
+		return (malloc(new_size));
 	if (new_size == 0 && ptr != NULL)
 	{
 	free(ptr);
-	return (NULL);
+		return (NULL);
 	}
 
 	new_ptr = malloc(new_size);
-
 	if (new_ptr == NULL)
 	{
-	return (NULL);
+	free(ptr);
+		return (NULL);
 	}
 
-	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+	if (new_size > old_size)
+	memcpy(new_ptr, ptr, old_size);
+	else
+	memcpy(new_ptr, ptr, new_size);
+
 	free(ptr);
 
-	return (new_ptr);
+		return (new_ptr);
 }
 
