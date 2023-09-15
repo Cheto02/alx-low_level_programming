ed300b190958e08d9aeae1e5cdca8a8629236520
Cheto02 <fredycheto@gmail.com>
2023-09-14 17:55:26 -0700

    100-main_opcodes.c betty corrections

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,32 +8,38 @@
  */
 void print_opcodes(unsigned char *opcodes, int count)
 {
-    int i;  // Declare the loop variable outside of the loop
+	int i;  /* Declare the loop variable outside of the loop */
 
-    for (i = 0; i < count; i++)
-    {
-        printf("%02x", opcodes[i]);
-        if (i < count - 1)
-            printf(" ");
-    }
+	for (i = 0; i < count; i++)
+	{
+	printf("%02x", opcodes[i]);
+	if (i < count - 1)
+		printf(" ");
+	}
 
-    printf("\n");
+	printf("\n");
 }
+/**
+ * main - program entry point
+ * Return: 0 on success
+ */
 
 int main(void)
 {
-    // Define the opcodes based on the provided assembly code
-    unsigned char main_opcodes[] = {
-        0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x30,
-        0x89, 0x7d, 0xdc, 0x48, 0x89, 0x75, 0xd0, 0x83,
-        0x7d, 0xdc, 0x02, 0x74, 0x14
-    };
+	/*Define the opcodes based on the provided assembly code */
+	unsigned char main_opcodes[] =
 
-    int opcode_count = sizeof(main_opcodes) / sizeof(main_opcodes[0]);
+	{
+	0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x30,
+	0x89, 0x7d, 0xdc, 0x48, 0x89, 0x75, 0xd0, 0x83,
+	0x7d, 0xdc, 0x02, 0x74, 0x14
+	};
 
-    // Print the opcodes in the requested format
-    print_opcodes(main_opcodes, opcode_count);
+	int opcode_count = sizeof(main_opcodes) / sizeof(main_opcodes[0]);
 
-    return 0;
+	/* rint the opcodes in the requested format */
+	print_opcodes(main_opcodes, opcode_count);
+
+	return (0);
 }
 
