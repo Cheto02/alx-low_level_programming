a5d1c158b1f1b1edcccca20b819e3a2553c72bba
Cheto02 <fredycheto@gmail.com>
2023-09-21 11:35:19 -0700

    update to 101-hello_holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,24 @@
 section .data
-    hello_message db "Hello, Holberton", 10, 0 ; 10 is newline character and 0 is null-terminator
+	hello_message db "Hello, Holberton", 10, 0 ; 10 is newline character and 0 is null-terminator
 
 section .text
-    extern printf
+	extern printf
 
-    global main
+	global main
 main:
-    ; Stack alignment
-    push rbp
-    mov rbp, rsp
-    and rsp, -16
+	; Stack alignment
+	push rbp
+	mov rbp, rsp
+	and rsp, -16
 
-    ; Prepare the arguments for printf
-    mov rdi, hello_message  ; 1st argument: pointer to format string
-    
-    ; Call printf
-    call printf
+	; Prepare the arguments for printf
+	mov rdi, hello_message  ; 1st argument: pointer to format string
 
-    ; Restore the stack and exit
-    mov rsp, rbp
-    pop rbp
-    ret
+	; Call printf
+	call printf
+
+	; Restore the stack and exit
+	mov rsp, rbp
+	pop rbp
+	ret
 
