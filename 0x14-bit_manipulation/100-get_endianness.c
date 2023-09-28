0c286693a12029104637f8bb869555ce97dcc13f
Cheto02 <fredycheto@gmail.com>
2023-09-28 14:12:35 -0700

    fixed char* to char *

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -10,7 +10,7 @@ int get_endianness(void)
 	unsigned int x = 1; /* Declare an unsigned int x and initialize it to 1 */
 	/* Declare a char pointer c and point it to the address of xi */
 	/* This is done to access the individual bytes of the integer */
-	char *c = (char*)&x;
+	char *c = (char *)&x;
 	/* Return the value pointed by c. If system is little endian */
 	return (*c);/*c will be 1. If system is big endian, *c will be 0 */
 }
