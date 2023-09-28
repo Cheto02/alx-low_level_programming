8ea89c2a0c49ecef0366e15cdfa38ef8a8bec2d4
Cheto02 <fredycheto@gmail.com>
2023-09-28 12:58:57 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that Prints the binary representation of a numbe
+ N*   @N  
+ * Return: nothing (void)
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1) /* If the number is greater than 1 */
+	/* Recursively call function with num shifted one bit to the right */
+	print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');/* Print last bit of num(0 or 1) as a char */
+}
