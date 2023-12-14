455625b1e923e1e57e0994f630566ea857599954
Cheto02 <fredycheto@gmail.com>
2023-12-14 10:58:56 -0800

    final check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,20 +12,24 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 
-	if (b == NULL)
+	int i = 0;
+
+	if (b == NULL || *b == '\0')
 	return (0);
 
-	while (*b)
+	while (b[i])
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] != '0' && b[i] != '1')
 		return (0);
-
-		num <<= 1;
-		if (*b == '1')
-		num += 1;
-		b++;
+		i++;
 	}
-
+	i = 0;
+	while (b[i])
+	{
+		num <<= 1;
+		num += b[i] - '0';
+		i++;
+	}
 	return (num);
 }
 
