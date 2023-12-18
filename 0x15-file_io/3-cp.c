b3ef4e96e04a54340a65d9115fe78a24945c6233
Cheto02 <fredycheto@gmail.com>
2023-12-18 12:34:17 -0800

    more changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,61 +1,69 @@
 #include "main.h"
-
-#define BUFFER_SIZE 1024
 /**
- * main - Entry point
- * @ac: Argument count
- * @av: Argument vector
- *
- * Return: 0 on success, 1 on incorrect argument count
+ * error_handler - Handles errors and exits the prog with the spec exit code
+ * @fd: File descriptor
+ * @str: Error message
+ * @exit_code: Exit code
  */
-int main(int ac, char **av)
+void error_handler(int fd, char *str, int exit_code)
 {
-	int res;
-
-	if (ac != 3)
-	{
-	dprintf(2, "Usage: %s filename src_file filename dst_file\n", av[0]);
-	exit(1);
-	}
-	res = copy_file(av[1], av[2]);
-	printf("-> %i)\n", res);
-	return (0);
+	if (fd == -1)
+	dprintf(2, "Error: %s\n", str);
+	else
+	dprintf(2, "Error: %s %d\n", str, fd);
+	exit(exit_code);
 }
 /**
  * copy_file - Copies the content of a file to another file
  * @src_file: The source file to read from
  * @dst_file: The destination file to write to
  *
- * Return: 1 on success, -1 on failure
+ * Return: 0 on success, 98 if unable to read, 99 if unable to write.
+ * 100 if unable to close file descriptors
  */
 int copy_file(const char *src_file, const char *dst_file)
 {
-	FILE *fp_src, *fp_dst;
-	char buffer[1024];
-	size_t bytes_read, bytes_written;
+	int src_fd, dst_fd;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
 
-	fp_src = fopen(src_file, "r");
-	fp_dst = fopen(dst_file, "w");
+	src_fd = open(src_file, O_RDONLY);
+	if (src_fd == -1)
+	error_handler(src_fd, "Can't read from file", 98);
 
-	if (fp_src == NULL || fp_dst == NULL)
+	dst_fd = open(dst_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dst_fd == -1)
+	error_handler(dst_fd, "Can't write to", 99);
+
+	while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0)
 	{
-	dprintf(2, "Error opening files %s and %s\n", src_file, dst_file);
-	return (-1);
-	}
-
-	while ((bytes_read = fread(buffer, 1, sizeof(buffer), fp_src)) > 0)
-	{
-		bytes_written = fwrite(buffer, 1, bytes_read, fp_dst);
+		bytes_written = write(dst_fd, buffer, bytes_read);
 		if (bytes_written != bytes_read)
-		{
-			dprintf(2, "Error writing %s\n", dst_file);
-			return (-1);
-		}
+		error_handler(dst_fd, "Can't write to", 99);
 	}
 
-	fclose(fp_src);
-	fclose(fp_dst);
+	if (bytes_read == -1)
+	error_handler(src_fd, "Can't read from file", 98);
 
-	return (1);
+	if (close(src_fd) == -1 || close(dst_fd) == -1)
+	error_handler(src_fd, "Can't close fd", 100);
+
+	return (0);
+}
+/**
+ * main - Entry point of the program
+ * @ac: Argument count
+ * @av: Argument vector
+ *
+ * Return: 0 on success, 97 on incorrect argument count
+ */
+int main(int ac, char **av)
+{
+	if (ac != 3)
+	error_handler(-1, "Usage: cp file_from file_to", 97);
+
+	copy_file(av[1], av[2]);
+
+	return (0);
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,9 +17,10 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-void error_exit(int code, char *message, char *file_name);
 int main(int ac, char **av);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(const char *src_file, const char *dst_file);
+void error_handler(int fd, char *str, int exit_code);
+
 
 #endif
