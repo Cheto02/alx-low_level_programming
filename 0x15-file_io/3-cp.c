7070fc35f6a22691b84f08d097a06a8114ae83ea
Cheto02 <fredycheto@gmail.com>
2023-12-18 09:55:44 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,82 +1,93 @@
 #include "main.h"
 
-/**
- * _putchar - Writes a character to stdout
- * @c: The character to print
- *
- * Return: On success 1, on error -1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+#define BUFFER_SIZE 1024
+
 
 /**
  * main - Entry point
  * @ac: Argument count
  * @av: Argument vector
  *
- * Return: 0 on success, 97, 98, 99, or 100 on failure
+ * Return: 0 on success, 97 on incorrect argument count
  */
 int main(int ac, char **av)
 {
-	int file_from, file_to, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
 	if (ac != 3)
-	error_exit(97, "Usage: cp file_from file_to\n", NULL);
-
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-	error_exit(98, "Error: Can't read from file ", av[1]);
-
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file_to == -1)
-	error_exit(99, "Error: Can't write to ", av[2]);
-
-	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(file_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		error_exit(99, "Error: Can't write to ", av[2]);
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
 	}
 
-	if (bytes_read == -1)
-	error_exit(98, "Error: Can't read from file ", av[1]);
+	return (append_text_to_file(av[1], av[2]));
+}
+/**
+ * append_text_to_file - Append the content of a file to another file
+ * @filename: The name of the file to read from
+ * @text_content: The name of the file to write to
+ *
+ * Return: 0 on success, -1 on failure, -100 on file descriptor closing failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd_file_from, fd_file_to;
+	int bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-	if (close(file_from) == -1)
-	error_exit(100, "Error: Can't close fd ", av[1]);
+	if (filename == NULL || text_content == NULL)
+	{
+		return (-1);
+	}
 
-	if (close(file_to) == -1)
-	error_exit(100, "Error: Can't close fd ", av[2]);
+	fd_file_from = open(filename, O_RDONLY);
+	if (fd_file_from == -1)
+	{
+		print_error_and_exit("Error: Can't read from file", filename, -1);
+	}
+
+	fd_file_to = open(text_content, O_WRONLY | O_CREAT, 0664);
+	if (fd_file_to == -1)
+	{
+		print_error_and_exit("Error: Can't create or open file", text_content, -1);
+	}
+
+	if (ftruncate(fd_file_to, 0) == -1)
+	{
+		print_error_and_exit("Error: Can't initialize file", text_content, -1);
+	}
+
+	bytes_read = read(fd_file_from, buffer, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		bytes_written = write(fd_file_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			print_error_and_exit("Error: Can't write to file", text_content, -1);
+		}
+		bytes_read = read(fd_file_from, buffer, BUFFER_SIZE);
+	}
+
+	if (close(fd_file_from) == -1)
+	{
+	print_error_and_exit("Error: Can't close file descriptor fd", "file_from", -100);
+	}
+
+	if (close(fd_file_to) == -1)
+	{
+	print_error_and_exit("Error: Can't close file descriptor fd", "file_to", -100);
+	}
 
 	return (0);
 }
-
 /**
- *error_exit - Display an error message and exit the program with a error code
- *@code: The error code to exit with
- *@message: The error message to display
- *@file_name: The name of the file related to the error (optional)
+ * print_error_and_exit - Print an ror mes and exit the proga spec exit code
+ * @error_message: The error message to print
+ * @file_name: The name of the file related to the error
+ * @exit_code: The exit code to exit with
  */
-
-void error_exit(int code, char *message, char *file_name)
+void print_error_and_exit(const char *error_message, const char *file_name, int exit_code)
 {
-	while (*message)
-	{
-		_putchar(*message);
-		message++;
-	}
-
-	if (file_name)
-	{
-		while (*file_name)
-		{
-			_putchar(*file_name);
-			file_name++;
-		}
-	}
-
-	exit(code);
+	dprintf(2, "%s %s\n", error_message, file_name);
+	exit(exit_code);
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,9 +17,10 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 void error_exit(int code, char *message, char *file_name);
 int main(int ac, char **av);
+void print_error_and_exit(const char *error_message, const char *file_name, int exit_code);
+int append_text_to_file(const char *filename, char *text_content);
 
 
 #endif
