ec2d662fecb6a72d62f1b1b7cc9a51815a447953
Cheto02 <fredycheto@gmail.com>
2023-12-18 10:53:42 -0800

    rmoved some proto to main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -19,7 +19,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 void error_exit(int code, char *message, char *file_name);
 int main(int ac, char **av);
-void print_error_and_exit(const char *error_message, const char *file_name, int exit_code);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(const char *src_file, const char *dst_file);
 
